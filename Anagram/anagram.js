f7fde8e827e4f9313c7a619009c8764acac8ee76
unknown <josefwambua2@gmail.com>
2024-11-15 12:48:02 +0300

    Anagram

--- Anagram/anagram.js
+++ Anagram/anagram.js
@@ -5,10 +5,24 @@
 // Input: (s = "anagram"), (t = "nagaram"); ---> output: true;
 // Input: (s - "rat"), (t  - "car");       ---> output: false;
 
-const isAnagram = function (s, t){
-    s  = s.split("");
+const isAnagram = function (s,t) {
+    // checking the lengths of the objects, should be of sane size
+
+    if(s.length !== t.length) return false;
+     // initializing empty objects
+     let obj1 = {}
+     let obj2 = {}
+
+     for (let i = 0; i<s.length; i++){
+        // comparing the two objects arrays
+        obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+        obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+     }
+     for (const key in obj1){
+      //checking if the keys are the same if not terminate it
+      if(obj1[key] !==  obj2[key]) return false;
+     }
+    return true;
 
 }
-
-//anagram - > xxxxxx;
-// nagaram => xxxxxx;
+console.log(isAnagram("plot","tar"));
